4d6b387304989625e6c781b30eab9a74f2b3e073
youweiMa <yom26@pitt.edu>
2020-08-10 23:47:55 +0800

    add RemoveDuplicates

---
+++ src/arrays101/deleteFromArray/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package arrays101.deleteFromArray;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        RemoveDuplicates test = new RemoveDuplicates();
+
+        int[] target = {1,1,2,2,3,3,4,4};
+        for(int i = 0; i < target.length; i++){
+            System.out.print(target[i] + " ");
+        }
+        System.out.println();
+
+        int result = test.removeDuplicates(target);
+        System.out.println(result);
+
+        for(int i = 0; i < result; i++){
+            System.out.print(target[i] + " ");
+        }
+    }
+
+    public int removeDuplicates(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++){
+            if(arr[i] != arr[j]) arr[++i] = arr[j];
+        }
+        return i+1;
+    }
+}
