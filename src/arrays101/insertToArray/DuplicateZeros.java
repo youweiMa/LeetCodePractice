9d760056139fa0403f8c7c725f46aca7904fb915
youweiMa <yom26@pitt.edu>
2020-08-12 23:16:30 +0800

    test update

---
+++ src/arrays101/insertToArray/DuplicateZeros.java
@@ -0,0 +1,39 @@
+package arrays101.insertToArray;
+
+public class DuplicateZeros {
+    public static void main(String[] args) {
+        DuplicateZeros test = new DuplicateZeros();
+        int[] nums = {1, 2, 0, 0, 3, 4, 0};         // expected: {1,2,0,0,0,0,3}
+        test.duplicateZeros(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public void duplicateZeros(int[] arr) {
+        int possibleDups = 0;
+        int lastIndex = arr.length - 1;
+        // find sections to be reserved
+        for (int left = 0; left <= lastIndex - possibleDups; left++) {
+            if (arr[left] == 0) {
+                // edge case
+                if (left == lastIndex - possibleDups) {
+                    arr[lastIndex] = 0;
+                    lastIndex--;
+                    break;
+                }
+                possibleDups++;
+            }
+        }
+        // copy from end to start
+        for (int right = lastIndex - possibleDups; right >= 0; right--) {
+            if (arr[right] == 0) {
+                arr[right + possibleDups] = 0;
+                possibleDups--;
+                arr[right + possibleDups] = 0;
+            } else {
+                arr[right + possibleDups] = arr[right];
+            }
+        }
+    }
+}
