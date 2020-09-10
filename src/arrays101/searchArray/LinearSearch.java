6e41548b39ff8c2822fd45fa5c82b196a0a8f364
youweiMa <yom26@pitt.edu>
2020-09-11 00:08:29 +0800

    add LinearSearch.java

---
+++ src/arrays101/searchArray/LinearSearch.java
@@ -0,0 +1,20 @@
+package arrays101.searchArray;
+
+public class LinearSearch {
+    public static boolean linearSearch(int[] array, int target) {
+        if (array.length == 0 || array == null) {
+            return false;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(linearSearch(list, 4));
+    }
+}
